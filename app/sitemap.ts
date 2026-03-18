3f36bab045f23274aa2f1bd5c917f0a5e52c46d5
unknown <arnab0574@gmail.com>
2026-03-18 15:20:21 +0600

    Fix the base url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://mubasshirarnab.vercel.app"; // Defaulting to vercel app url format or personal domain
+  const baseUrl = "https://mubasshirahmed.vercel.app"; // Defaulting to vercel app url format or personal domain
 
   return [
     {
